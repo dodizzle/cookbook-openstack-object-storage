c680de67b2e5dd1030c76638adb89b5df8452b57
David O'Dell <davidodell@gmail.com>
2016-11-17 12:18:51 -0800

    output raw data so that I can debug the parser

--- providers/ring_script.rb
+++ providers/ring_script.rb
@@ -77,7 +77,7 @@ def generate_script # rubocop:disable Metrics/AbcSize
     disk_data[:available][which] ||= {}
 
     disk_state.each do |swiftnode|
-      Chef::Log.info("#{which} node: #{swiftnode[:hostname]} state:\n#{PP.pp(swiftnode[:ipaddress])}")
+      Chef::Log.info("#{which} node: #{swiftnode[:hostname]} state:\n#{swiftnode[:ipaddress]}")
       if swiftnode['openstack']['object-storage']['state']['devs']
         swiftnode['openstack']['object-storage']['state']['devs'].each do |k, v|
           disk_data[which][v[:ip]] = disk_data[which][v[:ip]] || {}
