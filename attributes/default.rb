7c9dafa749a62d2f2dc8f0a1d9c71bf0cc5d9be1
David O'Dell <davidodell@gmail.com>
2016-12-05 15:42:11 -0800

    have each swift service log to its own file

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,22 +1,21 @@
-# swift_secret_databag_item# encoding: UTF-8
-#
-# Cookbook Name:: openstack-object-storage
-# Attributes:: default
-#
-# Copyright 2014 IBM Corp.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+default['opentack']['swift']['services'] = %w(
+  account-auditor
+  account-reaper
+  account-replicator
+  account-server
+  container-auditor
+  container-reconciler
+  container-replicator
+  container-server
+  container-sync
+  container-updater
+  object-auditor
+  object-replicator
+  object-server
+  object-updater
+  proxy-server
+  swift
+)
 
 default['openstack']['object-storage']['service_tenant_name'] = 'service'
 default['openstack']['object-storage']['service_user'] = 'swift'
---
+++ recipes/swift_syslog.rb
@@ -0,0 +1,15 @@
+service 'rsyslog'
+
+directory '/var/log/swift' do
+  action :create
+  owner 'syslog'
+  group 'adm'
+end
+
+template '/etc/rsyslog.d/51-swift.conf' do
+  source '51-swift.conf.erb'
+  variables(
+    swift_services: default['opentack']['swift']['services']
+  )
+  notifies :restart, 'service[rsyslog]', :delayed
+end
