f4b458d0561daacd0d1fc5183ca27626c009ef9e
David O'Dell <davidodell@gmail.com>
2017-01-13 15:44:21 -0800

    tagged last release for mitaka, now working in newton

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache 2.0'
 description 'Installs and configures Openstack Swift'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '13.0.0'
+version '14.0.0'
 
 recipe 'openstack-object-storage::account-server', 'Installs the swift account server'
 recipe 'openstack-object-storage::client', 'Install the swift client'
